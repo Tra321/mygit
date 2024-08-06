50f11e626711eeb02d8dc90f8f1f23b2c7377e8d
Atra <atraa0621@gmail.com>
2024-08-06 21:30:21 +0800

    vue

--- vue/V-project/my-app/src/views/Home.vue
+++ vue/V-project/my-app/src/views/Home.vue
@@ -38,7 +38,17 @@
                     </el-table>
                 </el-card>
         </el-col>
-        <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="16">
+            <div class="num">
+                <el-card v-for="item in countData" :key="item.name" :body-style="{ display: 'flex', padding: 0 }">
+                    <i class="icon" :class="`el-icon-${item.icon}`" :style="{ background: item.color }"></i>
+                    <div class="detail">
+                        <p class="price">￥{{ item.value }}</p>
+                        <p class="desc">{{ item.name }}</p>
+                    </div>
+                </el-card>
+            </div>
+        </el-col>
     </el-row>
 </template>
 <script>
@@ -88,7 +98,45 @@ export default {
                 todayBuy: '今日购买',
                 monthBuy: '本月购买',
                 totalBuy: '总购买'
-            }
+            },
+            countData: [
+                {
+                    name: "今日支付订单",
+                    value: 1234,
+                    icon: "success",
+                    color: "#2ec7c9",
+                },
+                {
+                    name: "今日收藏订单",
+                    value: 210,
+                    icon: "star-on",
+                    color: "#ffb980",
+                },
+                {
+                    name: "今日未支付订单",
+                    value: 1234,
+                    icon: "s-goods",
+                    color: "#5ab1ef",
+                },
+                {
+                    name: "本月支付订单",
+                    value: 1234,
+                    icon: "success",
+                    color: "#2ec7c9",
+                },
+                {
+                    name: "本月收藏订单",
+                    value: 210,
+                    icon: "star-on",
+                    color: "#ffb980",
+                },
+                {
+                    name: "本月未支付订单",
+                    value: 1234,
+                    icon: "s-goods",
+                    color: "#5ab1ef",
+                },
+            ]
         }
     }
 }
@@ -126,5 +174,39 @@ export default {
             margin-left: 60px;
         }
     }
+}
+.num {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .icon {
+        width: 80px;
+        height: 80px;
+        font-size: 30px;
+        text-align: center;
+        line-height: 80px;
+        color: #fff;
+    }
+    .detail {
+        margin-left: 15px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        .price {
+            font-size: 30px;
+            margin-bottom: 10px;
+            line-height: 30px;
+            height: 30px;
+        }
+        .desc {
+            font-size: 14px;
+            color: #999;
+            text-align: center;
+        }
+    }
+    .el-card {
+        width: 32%;
+        margin-bottom: 20px;
+    }    
 }
 </style>
