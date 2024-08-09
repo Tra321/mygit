23c8990700cc6cc37ce0ba981413fbf7c86f973a
Atra <atraa0621@gmail.com>
2024-08-09 21:31:40 +0800

    vue2

--- vue/V-project/my-app/src/router/index.js
+++ vue/V-project/my-app/src/router/index.js
@@ -6,6 +6,7 @@ import Main from '@/views/Main.vue'
 import Mall from '@/views/Mall.vue'
 import PageOne from '@/views/PageOne.vue'
 import PageTwo from '@/views/PageTwo.vue'
+import Login from '@/views/Login.vue'
 Vue.use(VueRouter)
 // 1、创建路由组件
 // 2、将路由与组件进行映射
@@ -25,6 +26,10 @@ const routes = [
             { path: 'page1', name: 'page1', component: PageOne }, // 页面1
             { path: 'page2', name: 'page2', component: PageTwo }, // 页面2
         ]
+    },
+    {
+        path: '/login',
+        component: Login
     }
 ]
 
---
+++ vue/V-project/my-app/src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <el-form label-width="70px" :inline="true" class="login-container" :model="form" :rules="rules">
+        <h3 class="login_title">系统登录</h3>
+        <el-form-item label="用户名" prop="username">
+            <el-input v-model="form.username" placeholder="请输入账号"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button style="margin-left: 105px;margin-top: 10px;" type="primary">登录</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            form:{
+                username: '',
+                password: ''
+            },
+            rules: {
+                username: [
+                    { required: true, trigger: 'blur', message: '请输入用户名' }
+                ],
+                password: [
+                    { required: true, trigger: 'blur', message: '请输入密码' }
+                ]
+            }
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.login-container {
+    width: 350px;
+    border: 1px solid #eaeaea;
+    margin: 180px auto;
+    padding: 35px 35px 15px 35px;
+    background-color: #fff;
+    border-radius: 15px;
+    box-shadow: 0 0 25px #cac6c6;
+    box-sizing: border-box;
+    .login_title {
+        text-align: center;
+        margin-bottom: 40px;
+        color: #505458;
+    }
+    .el-input {
+        width: 198px;
+    }
+}
+</style>
